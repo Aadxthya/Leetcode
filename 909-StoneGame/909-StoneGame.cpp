8a27eaa648fb774fefbd763a260604dc78886ed5
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:51:09 +0530

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.cpp
@@ -0,0 +1,21 @@
+// Last updated: 9/16/2025, 9:50:32 AM
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = piles[i]; // Base case: only one pile
+        }
+
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+
+        return dp[0][n-1] > 0;
+    }
+};
