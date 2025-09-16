192cc0a00cf4855a85af7b6fcc37698d1e79cd90
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:52:49 +0530

    Create 377-CombinationSumIv

---
+++ 377-CombinationSumIv/377-CombinationSumIv.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/16/2025, 9:52:11 AM
+class Solution {
+public:
+    int combinationSum4(std::vector<int>& nums, int target) {
+        std::vector<unsigned int> dp(target + 1, 0);
+        dp[0] = 1;
+        
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+};
