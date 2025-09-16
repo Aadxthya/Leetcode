877ef29344507d6b1c84a4cc4b0eeeb894b512ef
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:50:29 +0530

    Create 1511-CountNumberOfTeams

---
+++ 1511-CountNumberOfTeams/1511-CountNumberOfTeams.cpp
@@ -0,0 +1,37 @@
+// Last updated: 9/16/2025, 9:49:52 AM
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        
+        int n = rating.size();
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+
+            int leftsmaller = 0, leftgreater = 0;
+            int rightsmaller = 0, rightgreater = 0;
+
+            for(int j=0; j<i ; j++){
+                if(rating[j] > rating[i]){
+                    leftgreater++;
+                }
+                if(rating[j] < rating[i]){
+                    leftsmaller++;
+                }
+            }
+
+            for(int j=i+1; j<n ; j++){
+                if(rating[j] > rating[i]){
+                    rightgreater++;
+                }
+                if(rating[j] < rating[i]){
+                    rightsmaller++;
+                }
+            }
+
+            count += leftsmaller*rightgreater + leftgreater*rightsmaller;
+        }
+
+        return count;
+    }
+};
