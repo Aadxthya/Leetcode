89ec1992e8ed1540bdbb9221a6f9fbe743111077
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:50:24 +0530

    Create 1567-MaximumNumberOfVowelsInASubstringOfGivenLength

---
+++ 1567-MaximumNumberOfVowelsInASubstringOfGivenLength/1567-MaximumNumberOfVowelsInASubstringOfGivenLength.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/16/2025, 9:49:47 AM
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    int maxVowels(string s, int k) {
+        int maxVowel = 0, left = 0, vowel = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (isVowel(s[right])) vowel++;
+
+            if ((right - left + 1) == k) {
+                maxVowel = max(maxVowel, vowel);
+                if (isVowel(s[left])) vowel--;
+                left++;
+            }
+        }
+        return maxVowel;
+    }
+};
