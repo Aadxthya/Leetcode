b3c66dc9538e9e29f37fd9e677631cf7fed5f68f
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:51:20 +0530

    Create 871-KeysAndRooms

---
+++ 871-KeysAndRooms/871-KeysAndRooms.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/16/2025, 9:50:42 AM
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& adj) {
+        
+       int n = adj.size();
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+        int count = 1;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int key : adj[curr]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    count++;
+                    q.push(key);
+                }
+            }
+        }
+
+        return count == n;
+    }
+};
