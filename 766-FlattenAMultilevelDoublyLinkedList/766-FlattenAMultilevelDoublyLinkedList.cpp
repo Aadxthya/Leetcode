eed5e2f95dd7fd6f047744a7b26c66ac053415ec
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:51:39 +0530

    Create 766-FlattenAMultilevelDoublyLinkedList

---
+++ 766-FlattenAMultilevelDoublyLinkedList/766-FlattenAMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/16/2025, 9:51:02 AM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head, Node* rest = nullptr) {
+  if (!head) return rest;
+  head->next = flatten(head->child, flatten(head->next, rest));
+  if (head->next) head->next->prev = head;
+  head->child = nullptr;
+  return head;
+}
+};
