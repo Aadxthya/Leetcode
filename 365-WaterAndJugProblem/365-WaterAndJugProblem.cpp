d45ab5612fe17c04c0ba3d0a8d413d96bb3ada4a
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:52:52 +0530

    Create 365-WaterAndJugProblem

---
+++ 365-WaterAndJugProblem/365-WaterAndJugProblem.cpp
@@ -0,0 +1,34 @@
+// Last updated: 9/16/2025, 9:52:15 AM
+class Solution {
+public:
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        
+        int x=jug1Capacity,y=jug2Capacity,z=x+y;
+        int steps[]={x,-x,-y,y}; 
+        
+        queue<int> q;
+        vector<int> vis(z+1,0); 
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            
+            if(node==targetCapacity)
+            {
+                return true; // WHEN WE FIND THE TARGET CAPACITY ACHIEVED
+            }
+            for(int i=0;i<4;i++)
+            {
+                int newNode=node+steps[i];
+                if(newNode>=0 && newNode<=z && vis[newNode]==0) //BOUNDARY CHECKS 
+                {
+                    q.push(newNode);
+                    vis[newNode]=1;
+                }
+            }
+        }
+        return false; // IF TARGET CAPACITY CAN NEVER BE ACHIVED
+    }
+};
