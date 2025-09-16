63c92505bdaf0768a8adb20e0bc32880ba38c197
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:51:16 +0530

    Create 879-MaximizeDistanceToClosestPerson

---
+++ 879-MaximizeDistanceToClosestPerson/879-MaximizeDistanceToClosestPerson.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/16/2025, 9:50:38 AM
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int n = seats.size(), maxDist = 0;
+        int prev = -1;
+        for (int i = 0; i < n; ++i) {
+            if (seats[i]) {
+                if (prev == -1) maxDist = i;
+                else maxDist = max(maxDist, (i - prev) / 2);
+                prev = i;
+            }
+        }
+        maxDist = max(maxDist, n - 1 - prev);
+        return maxDist;
+    }
+};
