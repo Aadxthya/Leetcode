0f8b46c692bb8a8c9bbc4d90f9c1ac22178337d8
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:54:47 +0530

    Create 91-DecodeWays

---
+++ 91-DecodeWays/91-DecodeWays.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/16/2025, 9:54:09 AM
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if(s[0] - '0' == 0){
+            return 0;
+        }
+        vector<int> dp(n+2 , 0);
+        dp[0] =1;
+     
+        for(int i = 0 ; i< n ; i++){
+            
+          if(s[i] != '0'){        
+            dp[i+1] += dp[i];
+           if( i<n-1 && ((s[i] -'0')*10 + s[i+1] - '0') <=26 ){
+              dp[i+2] += dp[i];
+            }
+         }
+        }
+        return dp[n];
+    }
+};
