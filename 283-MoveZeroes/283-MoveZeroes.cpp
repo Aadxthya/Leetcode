a915295a98a469fcb46b1d0f1961d3e79dc4646d
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:53:07 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/16/2025, 9:52:29 AM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        for (int i= 0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[left]);
+                left++;
+            }
+
+        }
+            }
+        };       
+    
