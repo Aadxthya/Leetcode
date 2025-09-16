2af8721fd5c1def7501d62c189e36c55edb6b574
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:48:43 +0530

    Create 3555-FinalArrayStateAfterKMultiplicationOperationsI

---
+++ 3555-FinalArrayStateAfterKMultiplicationOperationsI/3555-FinalArrayStateAfterKMultiplicationOperationsI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/16/2025, 9:48:05 AM
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        while(k--){
+        int num = nums[0];
+        int index = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < num){
+                num = nums[i];
+                index = i;
+            }
+        }
+        nums[index] = nums[index]*multiplier;
+        }
+        return nums;
+    }
+};
