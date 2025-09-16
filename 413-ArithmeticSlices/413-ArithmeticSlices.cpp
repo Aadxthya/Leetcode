d2519a3ce69672cb0553e32c6c2b01058495e2c6
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:52:36 +0530

    Create 413-ArithmeticSlices

---
+++ 413-ArithmeticSlices/413-ArithmeticSlices.cpp
@@ -0,0 +1,25 @@
+// Last updated: 9/16/2025, 9:51:58 AM
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return 0;
+        vector<int> dp(n,0);
+
+        if(nums[2]-nums[1] == nums[1]-nums[0]){
+            dp[2] = 1;
+
+
+        }
+        int total = dp[2];
+
+        for(int i = 3; i < n ; i++){
+            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]){
+                 dp [i]= 1 + dp[i - 1];
+                  total += dp[i];
+            }
+
+        }
+         return total; 
+    }
+};
