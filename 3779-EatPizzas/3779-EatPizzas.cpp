032c1b3809b08ed8a7b2904ea7fb8afa6bae3ac5
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:48:32 +0530

    Create 3779-EatPizzas

---
+++ 3779-EatPizzas/3779-EatPizzas.cpp
@@ -0,0 +1,14 @@
+// Last updated: 9/16/2025, 9:47:54 AM
+class Solution {
+public:
+long long maxWeight(vector<int>& pizzas) {
+    long long ans = 0, totalDays = pizzas.size()/4, oddDays = (totalDays + 1)/2;
+    sort(pizzas.begin(), pizzas.end(), greater<int>());
+    for(int i = 0, day = 1; day <= totalDays; ++i, ++day){
+        if(day > oddDays) ++i;        //skip the largest, if we are pickking for the even days
+        ans += (long long)pizzas[i];
+    }
+    return ans;
+}       
+    
+};
