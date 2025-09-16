ab9a2d46b094d9541b5dd538ee8db13bf8b68a68
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:52:22 +0530

    Create 477-TotalHammingDistance

---
+++ 477-TotalHammingDistance/477-TotalHammingDistance.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2025, 9:51:44 AM
+
+class Solution {
+public:
+int totalHammingDistance(const vector<int>& nums) { 
+    int n = nums.size();
+    int total = 0;
+    // For each bit position (0 to 31)
+    for (int bit = 0; bit < 32; ++bit) {
+        int countOnes = 0;
+        for (int num : nums) {
+            countOnes += (num >> bit) & 1;
+        }
+        int countZeros = n - countOnes;
+        total += countOnes * countZeros;
+    }
+    return total;
+}
+};
