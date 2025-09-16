e82b9551c4cdc564d40ee9e7d543317ea1670ca8
Aadithya E <105816168+Aadxthya@users.noreply.github.com>
2025-09-16 09:53:32 +0530

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/16/2025, 9:52:54 AM
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* temp = curr->next; // Store the next node
+            curr->next = prev;            // Reverse the current node's pointer
+            prev = curr;                  // Move prev to current node
+            curr = temp;                  // Move to the next node
+        }
+        return prev; // New head of the reversed list
+    }
+};
